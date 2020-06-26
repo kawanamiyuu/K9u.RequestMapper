2ae391b3d5d121c1fba218243994e610a4d6a0cc
kawanamiyuu <i.am.really.you@gmail.com>
2020-06-26 20:08:44 +0900

    refactor: add interface

--- src/HandlerResolver.php
+++ src/HandlerResolver.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\RouteCollection as HandlerCollection;
 
-class HandlerResolver
+class HandlerResolver implements HandlerResolverInterface
 {
     private HandlerCollectorInterface $handlerCollector;
 
