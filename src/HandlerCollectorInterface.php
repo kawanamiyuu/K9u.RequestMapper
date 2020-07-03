922ee610d5e4e0cd443de475c07e3150685a4cfd
kawanamiyuu <i.am.really.you@gmail.com>
2020-07-03 22:30:30 +0900

    chore: add FIXME

--- src/HandlerCollectorInterface.php
+++ src/HandlerCollectorInterface.php
@@ -6,6 +6,9 @@ namespace K9u\RequestMapper;
 
 use Symfony\Component\Routing\RouteCollection as HandlerCollection;
 
+/**
+ * FIXME: this interface should be independent of Symfony's RouteCollection.
+ */
 interface HandlerCollectorInterface
 {
     public function __invoke(): HandlerCollection;
