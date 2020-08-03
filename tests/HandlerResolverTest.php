5916a1e8dbf836bd10e8cb64d30598ef70222953
kawanamiyuu <i.am.really.you@gmail.com>
2020-08-03 14:57:16 +0900

    chore: more tests

--- tests/HandlerResolverTest.php
+++ tests/HandlerResolverTest.php
@@ -16,24 +16,18 @@ class HandlerResolverTest extends TestCase
     private const FIXTURE_CACHE_DIR = __DIR__ . '/fixtures/cache';
     private const FIXTURE_HANDLER_DIR = __DIR__ . '/fixtures/handlers';
 
-    public function handlerResolverProvider(): iterable
-    {
-        yield [new HandlerResolver(self::FIXTURE_HANDLER_DIR)];
-        yield [new CachedHandlerResolver(self::FIXTURE_CACHE_DIR)];
-    }
-
     /**
-     * @dataProvider handlerResolverProvider
+     * @dataProvider dataProvider
      */
-    public function testResolve(HandlerResolverInterface $handlerResolver)
+    public function testResolve(HandlerResolverInterface $handlerResolver, array $request, array $expect)
     {
-        $request = $this->createRequest('GET', '/blogs/1');
+        $request = $this->createRequest($request[0], $request[1]);
 
         $handler = $handlerResolver($request);
 
-        $this->assertSame(BlogController::class, $handler->getClass());
-        $this->assertSame('get', $handler->getMethod());
-        $this->assertSame(['id' => '1'], $handler->getPathParams()->toArray());
+        $this->assertSame($expect[0], $handler->getClass());
+        $this->assertSame($expect[1], $handler->getMethod());
+        $this->assertSame($expect[2], $handler->getPathParams()->toArray());
     }
 
     /**
@@ -58,6 +52,48 @@ class HandlerResolverTest extends TestCase
         $request = $this->createRequest('DELETE', '/blogs');
 
         $handlerResolver($request);
+    }
+
+    public function handlerResolverProvider(): iterable
+    {
+        yield [new HandlerResolver(self::FIXTURE_HANDLER_DIR)];
+        yield [new CachedHandlerResolver(self::FIXTURE_CACHE_DIR)];
+    }
+
+    public function dataProvider(): iterable
+    {
+        $data = [
+            [
+                ['GET', '/blogs'],                   // request
+                [BlogController::class, 'index', []] // handler to be resolved
+            ],
+            [
+                ['GET', '/blogs/1'],
+                [BlogController::class, 'get', ['id' => '1']]
+            ],
+            [
+                ['POST', '/blogs'],
+                [BlogController::class, 'post', []]
+            ],
+            [
+                ['PUT', '/blogs/1'],
+                [BlogController::class, 'put', ['id' => '1']]
+            ],
+            [
+                ['PATCH', '/blogs/1'],
+                [BlogController::class, 'patch', ['id' => '1']]
+            ],
+            [
+                ['DELETE', '/blogs/1'],
+                [BlogController::class, 'delete', ['id' => '1']]
+            ]
+        ];
+
+        foreach ($this->handlerResolverProvider() as [$handlerResolver]) {
+            foreach ($data as [$request, $expect]) {
+                yield [$handlerResolver, $request, $expect];
+            }
+        }
     }
 
     private function createRequest(string $method, string $path): ServerRequestInterface
